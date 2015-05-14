a060925ea98ad345299452562ecb0f4db460140f
John Stevenson <john@jr0cket.co.uk>
2015-05-14 18:41:50 +0100

    Only serve content on the default route /
    If requests for other routes, such as /hello or /goodbye are recieved,
    then return a page not found response.

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,11 +2,15 @@
   (:require [ring.adapter.jetty :as jetty]))
 
 (defn greet
-  "A function to process all requests for the web server"
+  "A function to process all requests for the web server.  If a request is for something other than / then an error message is returned"
   [request]
-  {:status 200
-   :body "Hello, Clojure World"
-   :headers {}})
+  (if (= "/" (:uri request))
+    {:status 200
+     :body "Hello, Clojure World"
+     :headers {}}
+    {:status 404
+     :body "Sorry, page not found"
+     :headers {}}))
 
 (defn -main
   "A very simple web server using Ring & Jetty"
