bb6cd145767af058ebb454901c410528351ffe0e
John Stevenson <john@jr0cket.co.uk>
2015-05-14 17:12:45 +0100

    A running webserver using Jetty and Ring
    A very simple webserver using Jetty and Ring, launched with Leiningen
    and specfying a port number.
    
    lein run 8000

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,6 +1,12 @@
-(ns webdev.core)
+(ns webdev.core
+  (:require [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  "A very simple web server using Ring & Jetty"
+  [port-number]
+  (jetty/run-jetty
+     (fn [request] {:status 200
+                   :body "Hello, Clojure World"
+                   :headers {}})
+     {:port (Integer. port-number)}))
+
