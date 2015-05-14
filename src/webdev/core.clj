ea3084685ded435c67be6c7d566575860d596544
John Stevenson <john@jr0cket.co.uk>
2015-05-14 18:06:57 +0100

    Refactor request handling into greet function
    Handling different request routes will get complete the more routes are
    added, so we refactor out the route handling into its own function.
    
    Using a seperate function keeps the -main function easy to understand.

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,12 +1,16 @@
 (ns webdev.core
   (:require [ring.adapter.jetty :as jetty]))
 
+(defn greet
+  "A function to process all requests for the web server"
+  [request]
+  {:status 200
+   :body "Hello, Clojure World"
+   :headers {}})
+
 (defn -main
   "A very simple web server using Ring & Jetty"
   [port-number]
-  (jetty/run-jetty
-     (fn [request] {:status 200
-                   :body "Hello, Clojure World"
-                   :headers {}})
+  (jetty/run-jetty greet
      {:port (Integer. port-number)}))
 
