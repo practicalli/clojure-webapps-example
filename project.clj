612eec6553c40b11f079dd434cac3095dedbd658
John Stevenson <john@jr0cket.co.uk>
2015-05-15 23:17:52 +0100

    Change project to use Compojure
    Added dependencies for Compojure to project.clj file
    
    Updated code to use the defroutes macro and simplified the greet
    function to just say hello.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring "1.4.0-beta2"]]
+                 [ring "1.4.0-beta2"]
+                 [compojure "1.3.4"]]
   :main webdev.core
   :profiles {:dev
              {:main webdev.core/-dev-main}})
--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,33 +1,28 @@
 (ns webdev.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]))
 
 (defn greet
-  "A function to process all requests for the web server.  The default route / returns one message, /goodbye route another. for all other routes an error message is returned"
+  "Say hello to the world of Clojure"
   [request]
-  (cond
-   (= "/" (:uri request))
-   {:status 200
-    :body "Hello, Clojure World.  I now update automatically"
-    :headers {}}
-   (= "/goodbye" (:uri request))
-   {:status 200
-    :body "This is the end, my old friend"
-    :headers {}}
-   :else
-   {:status 404
-    :body "Sorry, page not found"
-    :headers {}}))
+  {:status 200
+   :body "Hello, Clojure World"
+   :headers {}})
+
+(defroutes app
+  (GET "/" [] greet)
+  (not-found "Sorry, page not found"))
 
 (defn -main
   "A very simple web server using Ring & Jetty"
   [port-number]
-  (jetty/run-jetty greet
+  (jetty/run-jetty app
      {:port (Integer. port-number)}))
 
 (defn -dev-main
   "A very simple web server using Ring & Jetty that reloads code changes via the development profile of Leiningen"
   [port-number]
-  (jetty/run-jetty (wrap-reload #'greet)
+  (jetty/run-jetty (wrap-reload #'app)
      {:port (Integer. port-number)}))
-
